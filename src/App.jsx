9274348d39ebe883dbf87fcce313bae30688fb02
znasriddinov <znasriddinov617@gmail.com>
2024-02-12 23:05:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Router>
+          <Routes>
+            <Route />
+            <Route />
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
+}
+
+export default App;
